244b0d0c8ec482ee6c940bf373d4ce031ba4d171
scuruchima1 <64672490+scuruchima1@users.noreply.github.com>
2021-04-18 10:45:29 -0500

    added full data set
    added full data set

--- forecaster.py
+++ forecaster.py
@@ -6,6 +6,7 @@ import pandas
 import matplotlib.pyplot as plt
 from sodapy import Socrata
 import config
+import multiprocessing
 
 def getavg(avglist, begin):
     output = []
@@ -70,11 +71,11 @@ def sevenMA(values, y, sevenday):
     else:
         return sevenday
 
-client = Socrata('data.cityofchicago.org', app_token = config.socratatoken, timeout = 3000)
+client = Socrata('data.cityofchicago.org', app_token = config.socratatoken, timeout = 1000)
 
 #Google trend data
 enddate = (datetime.now() - timedelta(days = 2)).strftime("%Y-%m-%d")
-trendlink = f'https://trends.google.com/trends/api/widgetdata/multiline/csv?req=%7B%22time%22%3A%222020-01-01%20{enddate}%22%2C%22resolution%22%3A%22WEEK%22%2C%22locale%22%3A%22en-US%22%2C%22comparisonItem%22%3A%5B%7B%22geo%22%3A%7B%22dma%22%3A%22602%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22covid%20symptoms%22%7D%5D%7D%7D%2C%7B%22geo%22%3A%7B%22dma%22%3A%22602%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22covid%20testing%20near%20me%22%7D%5D%7D%7D%5D%2C%22requestOptions%22%3A%7B%22property%22%3A%22%22%2C%22backend%22%3A%22IZG%22%2C%22category%22%3A0%7D%7D&token=APP6_UEAAAAAYHuhvzXTd_uuVJVhTL_xops6tpl6L3yc&tz=300'
+trendlink = f'https://trends.google.com/trends/api/widgetdata/multiline/csv?req=%7B%22time%22%3A%222020-01-01%202021-04-17%22%2C%22resolution%22%3A%22WEEK%22%2C%22locale%22%3A%22en-US%22%2C%22comparisonItem%22%3A%5B%7B%22geo%22%3A%7B%22dma%22%3A%22602%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22covid%20symptoms%22%7D%5D%7D%7D%2C%7B%22geo%22%3A%7B%22dma%22%3A%22602%22%7D%2C%22complexKeywordsRestriction%22%3A%7B%22keyword%22%3A%5B%7B%22type%22%3A%22BROAD%22%2C%22value%22%3A%22covid%20testing%20near%20me%22%7D%5D%7D%7D%5D%2C%22requestOptions%22%3A%7B%22property%22%3A%22%22%2C%22backend%22%3A%22IZG%22%2C%22category%22%3A0%7D%7D&token=APP6_UEAAAAAYH0JAQunEQTcLO-tsuP6exb6steLIgej&tz=300'
 
 download = requests.get(trendlink)
 decoded_content = download.content.decode()
@@ -82,36 +83,97 @@ cr = csv.reader(decoded_content.splitlines(), delimiter=',')
 trend = list(cr)
 #trend[>=3][0-2]
 
+print('Google trend data recieved')
+
 trend = getavg(trend, 4)
 x = derivelist(trend, 0)
 y = derivelist(trend, 1)
+trend1 = dict(zip(x, y))
 
 for i in range(len(y)):
     y[i] = y[i] * 10
 
-# #City of Chicago traffic data
+print('Google trend data processed')
+
+#City of Chicago traffic data
 carlimit = 10000000
-dailycars = client.get('kf7e-cur8', select = 'num_reads', order = 'time DESC', limit = carlimit)
-dailytime = client.get('kf7e-cur8', select = 'time', order = 'time DESC', limit = carlimit)
+dailycars = client.get('kf7e-cur8', select = 'num_reads', where = 'time > "2020-01-01"', order = 'time DESC', limit = carlimit)
+dailytime = client.get('kf7e-cur8', select = 'time', where = 'time > "2020-01-01"', order = 'time DESC', limit = carlimit)
+
+print('City of Chicago traffic data recieved')
+
 dailycars = getvalues(dailycars)[::-1]
 dailytime = getvalues(dailytime)[::-1]
 dailycartime = dict(zip(dailytime, dailycars))
 cardates, dailyaveragecars = dayavg(dailycartime)
+trend2 = dict(zip(cardates, dailyaveragecars))
+
+
+print('City of Chicago traffic data processed')
 
 #City of Chicago divvy bike data
 bikelimit = 100000000
-dailyavaildock = client.get('eq45-8inv', select = 'available_docks', order = 'timestamp DESC', limit = bikelimit)
-dailybiketime = client.get('eq45-8inv', select = 'timestamp', order = 'timestamp DESC', limit = bikelimit)
+startdate = datetime(2020,1,1)
+enddate = datetime(2021,4,17)
+delta = timedelta(days=1)
+dailyavaildock = []
+dailybiketime = []
+dailybiketime2 = []
+
+while startdate <= enddate:
+    dailyavaildock.extend(client.get('eq45-8inv',  select = 'available_docks', where = f'timestamp between "{startdate.strftime("%Y-%m-%d")}T12:00:00" and "{startdate.strftime("%Y-%m-%d")}T13:00:00"', order = 'timestamp DESC', limit = bikelimit))
+    dailybiketime.extend(client.get('eq45-8inv', select = 'timestamp', where = f'timestamp between "{startdate.strftime("%Y-%m-%d")}T12:00:00" and "{startdate.strftime("%Y-%m-%d")}T13:00:00"', order = 'timestamp DESC', limit = bikelimit))
+    dailyavaildock.extend(client.get('eq45-8inv',  select = 'available_docks', where = f'timestamp between "{startdate.strftime("%Y-%m-%d")}T17:00:00" and "{startdate.strftime("%Y-%m-%d")}T18:00:00"', order = 'timestamp DESC', limit = bikelimit))
+    dailybiketime.extend(client.get('eq45-8inv', select = 'timestamp', where = f'timestamp between "{startdate.strftime("%Y-%m-%d")}T17:00:00" and "{startdate.strftime("%Y-%m-%d")}T18:00:00"', order = 'timestamp DESC', limit = bikelimit))
+    print(startdate.strftime("%Y-%m-%d"))
+    dailybiketime2.append(str(startdate.strftime("%Y-%m-%d")))
+    startdate += delta
+
+print('City of Chicago divvy bike data recieved')
+
 dailyavaildock = getvalues(dailyavaildock)
 dailybiketime = getvalues(dailybiketime)
 dailyavailtime = dict(zip(dailybiketime,dailyavaildock))
 bikedates, dailyaveragebikes = dayavg(dailyavailtime)
+trend3 = dict(zip(dailybiketime2, dailyaveragebikes))
 
 for i in range(len(dailyaveragebikes)):
-    dailyaveragebikes[i] = dailyaveragebikes[i] * 10
+    dailyaveragebikes[i] = dailyaveragebikes[i] * 20 
+
+print('City of Chicago divvy bike data processed')
+
+#Data manipulation
+rows = []
+fields = ['Date', 'GTrend', 'Traffic', 'Bike']
+
+startdate = datetime(2020,1,1)
+enddate = datetime(2021,4,18)
+delta = timedelta(days=1)
+
+while startdate <= enddate:
+    temp = []
+    temp.append(str(startdate.strftime("%Y-%m-%d")))
+    try:
+        temp.append(trend1[str(startdate.strftime("%Y-%m-%d"))])
+    except Exception: 
+        temp.append("")
+    try:
+        temp.append(trend2[str(startdate.strftime("%Y-%m-%d"))])
+    except Exception: 
+        temp.append("")
+    try:
+        temp.append(trend3[str(startdate.strftime("%Y-%m-%d"))])
+    except Exception: 
+        temp.append("")
+    rows.append(temp)
+    startdate += delta
+
+with open('GFG', 'w') as f:
+    write = csv.writer(f)
+    write.writerow(fields)
+    write.writerows(rows)
 
 plt.plot(cardates, dailyaveragecars)
 plt.plot(x,y)
-plt.plot(bikedates, dailyaveragebikes)
+plt.plot(dailybiketime2, dailyaveragebikes)
 plt.show()
-
