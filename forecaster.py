6aae3608aa2aa8293f240b879975d758d1c8caac
scuruchima1 <64672490+scuruchima1@users.noreply.github.com>
2021-04-22 23:12:34 -0500

    Shortened and commented
    Shortened and commented
    Commented all parts of code
    Removed unnecessary code
    Removed unnecessary libraries

--- forecaster.py
+++ forecaster.py
@@ -1,121 +1,79 @@
 from pandas import read_csv
-from numpy import genfromtxt
-from numpy import delete
-from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-from sklearn import tree
 import csv
 
+#Training dataset 
 dataset = read_csv(r'data/avgshifted3.csv')
 
-# Create a validation dataset
+# Create a training dataset
 array = dataset.values
 X = array[:,2:]
 X=X.astype('int')
 y = array[:,1]
 y=y.astype('int')
 
-# Scale dataset
+# Scale independent variables
 scaler = StandardScaler()
 scaler.fit(X)
 X = scaler.transform(X)
 
+# Data split for training and validation 
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=693)
 
-# Make predictions on validation dataset
-# tr_score = []
-# ts_score = []
-
-# for j in range(1000):
-# 	X_train, X_test, y_train, y_test = train_test_split(X, y , random_state =j,     test_size=0.20)
-# 	model = DecisionTreeClassifier(random_state = 1)
-# 	model.fit(X_train, y_train)
-
-# 	tr_score.append(model.score(X_train, y_train))
-# 	ts_score.append(model.score(X_test, y_test))
-
-# J = ts_score.index(np.max(ts_score))
-# print(np.max(ts_score))
-# print(J)
-# X_train, X_test, y_train, y_test = train_test_split(X, y , random_state = 693, test_size=0.20)
-# model = DecisionTreeClassifier(random_state=693)
-# model.fit(X_train,y_train)
-# y_pred = model.predict(X_test)
-
-#693 68
-#2 3 15 19 26 28 31 35 87 89
-
+# Make decision tree classifier model with training data
 model = DecisionTreeClassifier(random_state=0)
 model.fit(X_train, Y_train)
-# scores.append(model.score(X_validation, Y_validation))
 
+# Data set for 3-day prediction
 test = read_csv(r'data/avg.csv')
 array1 = test.values
 X = array1[:-3,2:]
 X = X.astype('int')
 
+# Scale independent values for prediction data set
+scaler.fit(X)
+X = scaler.transform(X)
+
+# Real data set to add prediction values
 avgvalues = read_csv(r'data/trend.csv')
 array2 = avgvalues.values
 y = array2[:-3,1]
 y=y.astype('int')
 
-
-
-scaler.fit(X)
-X = scaler.transform(X)
-
+# Prediction results
 predictions = model.predict(X)
 
+# Append 3-day case predictions to real case data set
 lastvalues = predictions[-3:]
-print(lastvalues)
 for val in lastvalues:
 	y = np.append(y, val)
 
-# print(y)
+# Find 7-day moving average of real and prediction case data 
 y = np.convolve(y, np.ones(7), 'valid') / 7
+
+# Find 7-day moving average of raw predictions 
 predictions = np.convolve(predictions, np.ones(7), 'valid') / 7
-predictions = np.insert(predictions, 0,0, axis=0)
-predictions = np.insert(predictions, 0,0, axis=0)
-predictions = np.insert(predictions, 0,0, axis=0)
+# Append 0 at index 0 to offset 3 day gap and 7-day moving average
+for i in range(9):
+	predictions = np.insert(predictions, 0,0, axis=0)
 
-predictions = np.insert(predictions, 0,0, axis=0)
-predictions = np.insert(predictions, 0,0, axis=0)
-predictions = np.insert(predictions, 0,0, axis=0)
-predictions = np.insert(predictions, 0,0, axis=0)
-predictions = np.insert(predictions, 0,0, axis=0)
-predictions = np.insert(predictions, 0,0, axis=0)
-
+# Take 11 real case days + 3 predicted case days for data set
 predictedthree = y.tolist()
 predictedthree = predictedthree[-14:]
 predictedthree = np.array(predictedthree)
-print(predictedthree)
-# print(predictions)
-# pyplot.plot(predictions)
 predicted = np.reshape(predictedthree, (-1,1))
-print(predicted)
 writer = csv.writer(open(r'data/prediction.csv', 'w'))
 writer.writerows(predicted)
-predictedthree = predictedthree[-3:]
-pyplot.plot(y)
-pyplot.plot([len(y)-3,len(y)-2,len(y)-1],predictedthree)
-pyplot.show()
-#fix prediction days 
 
-# J = scores.index(np.max(scores))
-# print(np.max(scores))
-# print(J)
+#Take 3 predicted case days to graph
+predictedthree = predictedthree[-4:]
+
+#Plot data 
+pyplot.plot(y)
+pyplot.plot(predictions)
+pyplot.plot([len(y)-4,len(y)-3,len(y)-2,len(y)-1],predictedthree)
+pyplot.show()
